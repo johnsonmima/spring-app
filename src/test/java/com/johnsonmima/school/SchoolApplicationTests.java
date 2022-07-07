b4482a1329b8127a35525005c3e547b5b0fa68fe
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2022-07-07 12:51:39 -0400

    init

---
+++ src/test/java/com/johnsonmima/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnsonmima.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
